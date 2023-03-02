928e9c6b94c675563d78aea43d514c5c50c35a20
SamB207 <lovecita3@gmail.com>
2023-03-01 21:33:02 -0500

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './me.jpg';
+import logo from './Samantha.jpg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+
           My name is Samantha and I'm going to master React!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/SamB207"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check Me Out!
         </a>
       </header>
     </div>
